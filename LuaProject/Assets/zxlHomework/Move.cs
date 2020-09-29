1988d7b1d02bfa3234de187a8e48dc53aae4ca33
zhangxinlu <843979462@qq.com>
2020-09-29 18:24:03 +0800

    0929Luahomework_zxl

---
+++ LuaProject/Assets/zxlHomework/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class Move : MonoBehaviour
+{
+    private LuaEnv luaenv = new LuaEnv();
+
+
+    [CSharpCallLua]
+    private delegate void Del();
+    [CSharpCallLua]
+    private delegate void Set(Move move);
+    private Del update;
+
+    void Start()
+    {
+        luaenv.DoString("require 'zxlMove'");
+        Set set = luaenv.Global.Get<Set>("Set");
+        set(this);
+        update = luaenv.Global.Get<Del>("Update");
+    }
+
+    void Update()
+    {
+        if (null != update)
+        {
+            update();
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        update = null;
+        luaenv.Dispose();
+    }
+}
---
+++ LuaProject/Assets/zxlHomework/zxlHelloworld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using XLua;
+
+namespace XLuaTest
+{
+    public class zxlHelloworld : MonoBehaviour
+    {
+        void Start()
+        {
+            LuaEnv luaenv = new LuaEnv();
+            luaenv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+            luaenv.Dispose();
+        }
+    }
+}
